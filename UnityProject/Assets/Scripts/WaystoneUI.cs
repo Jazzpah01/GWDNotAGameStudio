b3c62b3f50904cc9378365ae051a5eda7a20d37f
ITUJesperR <90392730+ITUJesperR@users.noreply.github.com>
2022-03-18 15:38:28 +0100

    Setting up menu item player glyph populate

--- UnityProject/Assets/Scripts/WaystoneUI.cs
+++ UnityProject/Assets/Scripts/WaystoneUI.cs
@@ -12,22 +12,59 @@ public class WaystoneUI : MonoBehaviour
     public InteractableUI landscapeSlot;
     public InteractableUI biomeSlot;
 
+    public GameObject playerItemPrefab;
+
     public static WaystoneUI instance;
+
+    private Glyph currentGlyph;
+    private List<InteractableUI> playerItems;
+    
 
     private void Awake()
     {
         instance = this;
     }
 
+    private void Start()
+    {
+        PopulateMenuItems();
+
+        // Delegate button presses
+        for (int i = 0; i < playerItems.Count; i++)
+        {
+            int index = i;
+            playerItems[i].OnClicked += delegate { SelectGlyph(GlyphManager.playerGlyphs[index]); };
+        }
+
+        landscapeSlot.OnClicked += delegate { SetGlyph(currentGlyph); };
+        biomeSlot.OnClicked += delegate { SetGlyph(currentGlyph); };
+
+        submitButton.GetComponent<InteractableUI>().OnClicked += delegate { Submit(); };
+    }
+
+    void PopulateMenuItems()
+    {
+        playerItems = new List<InteractableUI>();
+
+        for (int i = 0; i < GlyphManager.playerGlyphs.Count; i++)
+        {
+            GameObject go = Instantiate(playerItemPrefab, playerGlyphs.transform);
+            playerItems.Add(go.GetComponent<InteractableUI>());
+        }
+    }
+
     public void SelectGlyph(Glyph g)
     {
+        currentGlyph = g;
         if (g is GlyphBiome)
         {
+            GlyphManager.biome = (GlyphBiome)g;
             landscapeSlot.Interactable = false;
             biomeSlot.Interactable = true;
         }
         else if (g is GlyphLandscape)
         {
+            GlyphManager.landscape = (GlyphLandscape)g;
             biomeSlot.Interactable = false;
             landscapeSlot.Interactable = true;
         } else
@@ -47,4 +84,42 @@ public class WaystoneUI : MonoBehaviour
             landscapeSlot.Sprite = g.icon;
         }
     }
+
+    public void Submit()
+    {
+        if (GlyphManager.biome == null || GlyphManager.landscape == null)
+        {
+            Debug.Log("Invalid glyphs");
+            return;
+        }
+
+        LevelManager.instance.ChangeScene();
+    }
+
+    //public void SelectGlyph(Glyph g)
+    //{
+    //    Debug.Log("Glyph selected!");
+    //    currentGlyph = g;
+    //    waystoneUI.SelectGlyph(g);
+    //}
+
+    //public void SetGlyph(Glyph g)
+    //{
+    //    Debug.Log("Glyph set!");
+    //    waystoneUI.SetGlyph(g);
+    //    if (g is GlyphLandscape)
+    //    {
+    //        GlyphManager.landscape = (GlyphLandscape)g;
+    //    }
+    //    else if (g is GlyphBiome)
+    //    {
+    //        GlyphManager.biome = (GlyphBiome)g;
+    //    }
+    //    else if (g is GlyphTime)
+    //    {
+    //        GlyphManager.time = (GlyphTime)g;
+    //    }
+    //}
+
+
 }

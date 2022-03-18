4f2ad531310ec4f5f7639228f9d2d9bbdc3582db
Rasmus Klintsø <rall1812@gmail.com>
2022-03-18 16:31:06 +0100

    wip moving cloud stuff

--- UnityProject/Assets/Scripts/LevelManager.cs
+++ UnityProject/Assets/Scripts/LevelManager.cs
@@ -24,14 +24,7 @@ public class LevelManager : MonoBehaviour
     [Header("Debug")]
     public SpawnRegion[] backgroundRegions;
 
-    // cloud stuff
-    public GameObject cloud_prefab;
-    public Vector3 cloud_spawn; // TODO: decide cloud spawn position
-    private float cloud_timer;
-    public float cloud_interval;
-    public int cloud_capacity;
-    public int clouds_active;
-
+    
     private void Awake()
     {
         instance = this;
@@ -46,27 +39,12 @@ public class LevelManager : MonoBehaviour
 
         PopulateScene();
 
-        cloud_timer = 0f;
+        
     }
 
     private void Update()
     {
-        // cloud spawning
-        cloud_timer += Time.deltaTime;
-        if (GameManager.instance.player != null && cloud_prefab != null && cloud_timer > cloud_interval && clouds_active <= cloud_capacity)
-        {
-            Vector3 playerPos = GameManager.instance.player.transform.position;
-            Vector3 spawnPos = new Vector3(playerPos.x - 10f, playerPos.y + 5f, 0f);    // TODO: replace magic numbers with camera dimensions
-            GameObject cloud = Instantiate(cloud_prefab);
-            cloud.transform.position = spawnPos;
-            clouds_active++;
-            cloud_timer = 0f;
-            Debug.Log("Cloud Spawned! (pos: " + spawnPos + " ) - Number of active clouds = " + clouds_active);
-        } 
-        else if (cloud_timer > cloud_interval) 
-        {
-            cloud_timer = 0f;
-        }
+        
     }
 
     public void PopulateScene()

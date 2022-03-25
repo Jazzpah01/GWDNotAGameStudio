de391afa441bbfe0cb2f23e0160786a4cb53e0e5
ITUJesperR <90392730+ITUJesperR@users.noreply.github.com>
2022-03-25 14:24:42 +0100

    debug sprite

--- UnityProject/Assets/DebugSprite.cs
+++ UnityProject/Assets/DebugSprite.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class DebugSprite : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(GetComponent<SpriteRenderer>());
+        Destroy(this);
     }
 }

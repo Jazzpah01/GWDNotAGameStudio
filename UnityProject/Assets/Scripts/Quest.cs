f60d8a330602662cf5b36696cd1b3e523ed6cd37
Rasmus Klintsø <rall1812@gmail.com>
2022-04-06 13:08:11 +0200

    added wip scripts for progression system

---
+++ UnityProject/Assets/Scripts/Quest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour
+{
+
+    // list of quest events
+
+    // execute quest events on a specific trigger based on scene objects
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

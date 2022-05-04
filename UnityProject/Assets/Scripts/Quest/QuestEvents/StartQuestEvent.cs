1a24c4fd316dfb536cb0ada18bb9cf81b53c1e20
ITUJesperR <90392730+ITUJesperR@users.noreply.github.com>
2022-05-04 19:37:16 +0200

    initial quest

---
+++ UnityProject/Assets/Scripts/Quest/QuestEvents/StartQuestEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartQuestEvent : QuestEvent
+{
+    public override QuestEventType questEventType => throw new System.NotImplementedException();
+
+    public override void Execute(SceneContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool ShouldExecute(SceneContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+}

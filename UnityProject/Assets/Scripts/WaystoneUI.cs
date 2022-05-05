471a5ce575f962d321ae125fe44ece609be7ea6b
Rasmus Klintsø <rall1812@gmail.com>
2022-05-05 22:08:06 +0200

    split glyphs in UI

--- UnityProject/Assets/Scripts/WaystoneUI.cs
+++ UnityProject/Assets/Scripts/WaystoneUI.cs
@@ -8,6 +8,8 @@ public class WaystoneUI : MonoBehaviour
 
 
     public GameObject playerGlyphs;
+    public GameObject biomeUI;
+    public GameObject landscapeUI;
     public GameObject waystoneGlyphs;
     public GameObject submitButton;
 
@@ -19,7 +21,9 @@ public class WaystoneUI : MonoBehaviour
     public static WaystoneUI instance;
 
     private Glyph currentGlyph;
-    private List<InteractableUI> playerItems;
+    //private List<InteractableUI> playerItems;
+    private List<(InteractableUI,int)> UI_biomeGlyphs;
+    private List<(InteractableUI,int)> UI_landscapeGlyphs;
 
     private InteractableUI oldToggled;
 
@@ -58,6 +62,7 @@ public class WaystoneUI : MonoBehaviour
         PopulateMenuItems();
     }
 
+    /*
     void CleanupMenuItems()
     {
         if (playerItems == null)
@@ -71,7 +76,29 @@ public class WaystoneUI : MonoBehaviour
 
         playerItems.Clear();
     }
+    */
+    void CleanupMenuItems()
+    {
+        if (UI_biomeGlyphs == null && UI_landscapeGlyphs == null)
+            return;
 
+        foreach ((InteractableUI,int) item in UI_biomeGlyphs.ToArray())
+        {
+            item.Item1.OnClicked = null;
+            Destroy(item.Item1.gameObject);
+        }
+
+        foreach ((InteractableUI, int) item in UI_landscapeGlyphs.ToArray())
+        {
+            item.Item1.OnClicked = null;
+            Destroy(item.Item1.gameObject);
+        }
+        
+        UI_biomeGlyphs.Clear();
+        UI_landscapeGlyphs.Clear();
+    }
+
+    /*
     void PopulateMenuItems()
     {
         playerItems = new List<InteractableUI>();
@@ -98,6 +125,81 @@ public class WaystoneUI : MonoBehaviour
             };
         }
     }
+    */
+
+    void PopulateMenuItems()
+    {
+        //playerItems = new List<InteractableUI>();
+        UI_biomeGlyphs = new List<(InteractableUI,int)>();
+        UI_landscapeGlyphs = new List<(InteractableUI, int)>();
+
+        // Create UI elements for player items
+        for (int i = 0; i < GlyphManager.playerGlyphs.Count; i++)
+        {
+            //GameObject go = Instantiate(playerItemPrefab, playerGlyphs.transform);
+            //InteractableUI iui = go.GetComponent<InteractableUI>();
+            //iui.Sprite = GlyphManager.playerGlyphs[i].icon;
+            //playerItems.Add(iui);
+            //if (GlyphManager.playerGlyphs[i] is GlyphBiome) UI_biomeGlyphs.Add((iui, i));
+            //if (GlyphManager.playerGlyphs[i] is GlyphLandscape) UI_landscapeGlyphs.Add((iui, i));
+
+            if (GlyphManager.playerGlyphs[i] is GlyphBiome)
+            {
+                GameObject go = Instantiate(playerItemPrefab, biomeUI.transform);
+                InteractableUI iui = go.GetComponent<InteractableUI>();
+                iui.Sprite = GlyphManager.playerGlyphs[i].icon;
+                UI_biomeGlyphs.Add((iui, i));
+            }
+
+            if (GlyphManager.playerGlyphs[i] is GlyphLandscape)
+            {
+                GameObject go = Instantiate(playerItemPrefab, landscapeUI.transform);
+                InteractableUI iui = go.GetComponent<InteractableUI>();
+                iui.Sprite = GlyphManager.playerGlyphs[i].icon;
+                UI_landscapeGlyphs.Add((iui, i));
+            }
+        }
+
+        // Delegate button presses for player items
+        //for (int i = 0; i < playerItems.Count; i++)
+        //{
+        //    int index = i;
+        //    playerItems[i].OnClicked += delegate {
+        //        SelectGlyph(GlyphManager.playerGlyphs[playerItems[index].]);
+        //        playerItems[index].Toggled = true;
+        //        if (oldToggled != null)
+        //            oldToggled.Toggled = false;
+        //        oldToggled = playerItems[index];
+        //    };
+        //}
+
+        for (int i = 0; i < UI_landscapeGlyphs.Count; i++)
+        {
+            int j = i;
+            UI_landscapeGlyphs[i].Item1.OnClicked += delegate
+            {
+                SelectGlyph(GlyphManager.playerGlyphs[UI_landscapeGlyphs[j].Item2]);
+                UI_landscapeGlyphs[j].Item1.Toggled = true;
+                if (oldToggled != null)
+                    oldToggled.Toggled = false;
+                oldToggled = UI_landscapeGlyphs[j].Item1;
+            };
+        }
+
+        for (int i = 0; i < UI_biomeGlyphs.Count; i++)
+        {
+            int j = i;
+            UI_biomeGlyphs[i].Item1.OnClicked += delegate
+            {
+                SelectGlyph(GlyphManager.playerGlyphs[UI_biomeGlyphs[j].Item2]);
+                UI_biomeGlyphs[j].Item1.Toggled = true;
+                if (oldToggled != null)
+                    oldToggled.Toggled = false;
+                oldToggled = UI_biomeGlyphs[j].Item1;
+            };
+        }
+    }
+
 
     public void SelectGlyph(Glyph g)
     {
